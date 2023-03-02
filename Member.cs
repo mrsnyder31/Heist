9b05c96e949ac789cfc4928386bd44f3006bf9e7
Marek Snyder <mr.snyder31@gmail.com>
2023-03-02 17:07:33 -0500

    phase 2 complete

--- Member.cs
+++ Member.cs
@@ -2,6 +2,13 @@ namespace Heist
 {
     class Member
     {
+
+        public Member(string newName, int newSkill, double newCourage)
+        {
+            Name = newName;
+            SkillLevel = newSkill;
+            Courage = newCourage;
+        }
         public string Name { get; set; }
         public int SkillLevel { get; set; }
         public double Courage { get; set; }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Heist
 {
@@ -6,8 +7,12 @@ namespace Heist
     {
         static void Main(string[] args)
         {
+
+            List<Member> HeistCrew = new List<Member>();
+
             Console.WriteLine("Plan Your Heist!");
 
+        another:
             Console.WriteLine("What is in your team member's name?");
             string name = Console.ReadLine();
 
@@ -21,6 +26,31 @@ namespace Heist
 Name: {name},
 Skill Level: {skill} 
 Courage Factor: {courage}");
+
+            Member member = new Member(name, skill, courage);
+            HeistCrew.Add(member);
+
+            Console.WriteLine("Do you need another member? Yes/No");
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes")
+            {
+                goto another;
+            }
+            else
+            {
+                Console.WriteLine("------------");
+                Console.Clear();
+                Console.WriteLine($"You have {HeistCrew.Count} team member.");
+            }
+
+            foreach (var crewMem in HeistCrew)
+            {
+                Console.WriteLine(@$"
+Name: {crewMem.Name},
+Skill Level: {crewMem.SkillLevel} 
+Courage Factor: {crewMem.Courage}");
+                Console.WriteLine("-------------");
+            }
         }
     }
 }
