48de1f9a274a0714b2732406804fe85b39f3da11
Marek Snyder <mr.snyder31@gmail.com>
2023-03-02 16:35:25 -0500

    phase 1 complete

---
+++ Member.cs
@@ -0,0 +1,9 @@
+namespace Heist
+{
+    class Member
+    {
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+        public double Courage { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace Heist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Plan Your Heist!");
+
+            Console.WriteLine("What is in your team member's name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"What is {name}'s skill level? (1-10)");
+            int skill = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"How couragous is {name}? (0.0 - 2.0)");
+            double courage = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(@$"
+Name: {name},
+Skill Level: {skill} 
+Courage Factor: {courage}");
         }
     }
 }
