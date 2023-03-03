db93a1ccf24a19a414a0a923c2ffcdee14bef2f8
Marek Snyder <mr.snyder31@gmail.com>
2023-03-03 10:31:00 -0500

    phase 3 complete

--- Program.cs
+++ Program.cs
@@ -13,10 +13,11 @@ namespace Heist
             Console.WriteLine("Plan Your Heist!");
 
         another:
-            Console.WriteLine("What is in your team member's name?");
+            Console.WriteLine("");
+            Console.WriteLine("What is your team member's name?");
             string name = Console.ReadLine();
 
-            Console.WriteLine($"What is {name}'s skill level? (1-10)");
+            Console.WriteLine($"What is {name}'s skill level? (1-100)");
             int skill = int.Parse(Console.ReadLine());
 
             Console.WriteLine($"How couragous is {name}? (0.0 - 2.0)");
@@ -30,7 +31,7 @@ Courage Factor: {courage}");
             Member member = new Member(name, skill, courage);
             HeistCrew.Add(member);
 
-            Console.WriteLine("Do you need another member? Yes/No");
+            Console.WriteLine("Do you need another member? (Yes/No)");
             string answer = Console.ReadLine().ToLower();
             if (answer == "yes")
             {
@@ -43,14 +44,33 @@ Courage Factor: {courage}");
                 Console.WriteLine($"You have {HeistCrew.Count} team member.");
             }
 
+            //             foreach (var crewMem in HeistCrew)
+            //             {
+            //                 Console.WriteLine(@$"
+            // Name: {crewMem.Name},
+            // Skill Level: {crewMem.SkillLevel} 
+            // Courage Factor: {crewMem.Courage}");
+            //                 Console.WriteLine("-------------");
+            //             }
+
+            int BankSecurity = 100;
+
+            int CrewSkill = 0;
+
             foreach (var crewMem in HeistCrew)
             {
-                Console.WriteLine(@$"
-Name: {crewMem.Name},
-Skill Level: {crewMem.SkillLevel} 
-Courage Factor: {crewMem.Courage}");
-                Console.WriteLine("-------------");
+                CrewSkill += crewMem.SkillLevel;
             }
+
+            if (CrewSkill >= BankSecurity)
+            {
+                Console.WriteLine("The heist was a success! You made a clean getaway.");
+            }
+            else
+            {
+                Console.WriteLine($"{HeistCrew[0].Name} tripped the alarm! You were all arrested.");
+            }
+
         }
     }
 }
